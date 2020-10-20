7f518ca12af2b2a6a0904ce6fc250ededc79d7f9
Niv <dindibo4@gmail.com>
2020-10-20 22:39:51 +0300

    WIP: Adding patching feature

--- elf-parser-main.c
+++ elf-parser-main.c
@@ -1,4 +1,152 @@
 #include <elf-parser.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+Elf32_Shdr *findSectionByName(const char *sectionName, int32_t fd, Elf32_Ehdr eh, Elf32_Shdr sh_table[]){
+	uint32_t i;
+	char* sh_str;	/* section-header string-table is also a section. */
+
+	sh_str = read_section(fd, sh_table[eh.e_shstrndx]);
+
+	for(i=0; i<eh.e_shnum; i++) {
+		
+		// If section was found
+		if(strcmp(sectionName, (sh_str + sh_table[i].sh_name)) == 0){
+			return &sh_table[i];
+		}
+	}
+
+	return NULL;
+}
+
+void makeSectionExecutable(const char *sectionName, int32_t fd, Elf32_Ehdr eh, Elf32_Shdr sh_table[]){
+	uint32_t i;
+	char* sh_str;	/* section-header string-table is also a section. */
+
+	sh_str = read_section(fd, sh_table[eh.e_shstrndx]);
+
+	for(i=0; i<eh.e_shnum; i++) {
+		
+		// If section was found
+		if(strcmp(sectionName, (sh_str + sh_table[i].sh_name)) == 0){
+			sh_table[i].sh_flags |= 0b100;
+			break;
+		}
+	}
+}
+
+char *getPatchedName(const char *orgName){
+	static const char *EXTENSION = ".patch";
+
+	int buffSize = strlen(orgName) + 1 + strlen(EXTENSION);
+	char *newBuffer = (char *)malloc(buffSize);
+	strncpy(newBuffer, orgName, buffSize);
+	strcat(newBuffer, EXTENSION);
+
+	return newBuffer;
+}
+
+bool duplicateFile(const char *orgFilename, char *newFilename){
+	int fds[2];
+	int bytesRead;
+	char buff[2048];
+
+	fds[0] = open(orgFilename, O_RDONLY);
+	fds[1] = open(newFilename, O_CREAT | O_RDWR);
+
+	while((bytesRead = read(fds[0], buff, sizeof(buff))) > 0){
+		if(!write(fds[1], buff, bytesRead)){
+			return false;
+		}
+	}
+
+	chmod(newFilename, 0664);
+
+	return true;
+}
+
+int createPatch(const char *orgName){
+	char *name = getPatchedName(orgName);
+	int fd = open(name, O_RDWR | O_CREAT);
+
+	return fd;
+}
+
+void writeFileHeader(int fd, Elf32_Ehdr *eh){
+	assert(eh != NULL);
+	assert(lseek(fd, (off_t)0, SEEK_SET) == (off_t)0);
+	assert(write(fd, (void *)eh, sizeof(Elf32_Ehdr)) == sizeof(Elf32_Ehdr));
+}
+
+void writeSectionTable(int32_t fd, Elf32_Ehdr *eh, Elf32_Shdr sh_table[]){
+	uint32_t i;
+
+	assert(lseek(fd, (off_t)eh->e_shoff, SEEK_SET) == (off_t)eh->e_shoff);
+
+	for(i=0; i<eh->e_shnum; i++) {
+		assert(write(fd, (void *)&sh_table[i], eh->e_shentsize)
+				== eh->e_shentsize);
+	}
+}
+
+char *extractCodeSection(int32_t fd, Elf32_Ehdr eh, Elf32_Shdr sh_table[], int *readSize){
+	Elf32_Shdr *codeSec = findSectionByName(".text", fd, eh, sh_table);
+	int secSize = codeSec->sh_size;
+	char *codeBuffer = (char *)malloc(secSize);
+	
+	assert(lseek(fd, (off_t)codeSec->sh_offset, SEEK_SET) == (off_t)codeSec->sh_offset);
+	read(fd, codeBuffer, secSize);
+
+	if(readSize != NULL ){
+		*readSize = secSize;
+	}
+
+	return codeBuffer;
+}
+
+char *findLongestCave(char *codeSec, int secSize, int *codecaveSize){
+	static const int MIN_LENGTH = 9;
+
+	int 	bytesIter		= 0;
+	int 	currentSize		= 0;
+	int 	biggest			= 0;
+	char 	*longest		= NULL;
+	char 	*beggining		= NULL;
+
+	for(char *ptr = codeSec; bytesIter < secSize; ptr++, bytesIter++){
+		if(*ptr == '\00'){
+			if(currentSize == 0){
+				beggining = ptr;
+			}
+
+			currentSize++;
+
+			if(currentSize > biggest){
+				biggest = currentSize;
+				longest = beggining;
+			}
+		}
+		else{
+			currentSize = 0;
+			beggining = NULL;
+		}
+	}
+
+	if(biggest <= MIN_LENGTH){
+		printf("biggest was %d :(\n", biggest);
+		biggest = 0;
+		longest = NULL;
+	}
+
+	if(codecaveSize != NULL){
+		*codecaveSize = biggest;
+	}
+
+	return longest;
+}
 
 /* Main entry point of elf-parser */
 int32_t main(int32_t argc, char *argv[])
@@ -62,12 +210,42 @@ int32_t main(int32_t argc, char *argv[])
 
 		/* Section header table :  */
 		sh_tbl = malloc(eh.e_shentsize * eh.e_shnum);
+		
 		if(!sh_tbl) {
 			printf("Failed to allocate %d bytes\n",
 					(eh.e_shentsize * eh.e_shnum));
 		}
+
 		read_section_header_table(fd, eh, sh_tbl);
 		print_section_headers(fd, eh, sh_tbl);
+
+		puts("END");
+
+		char *patchName = getPatchedName(argv[1]);
+		duplicateFile(argv[1], patchName);
+		int patchFD = open(patchName, O_RDWR);
+		
+		//writeFileHeader(patchFD, &eh);
+		//writeSectionTable(patchFD, &eh, sh_tbl);
+		makeSectionExecutable(".data", fd, eh, sh_tbl);
+		writeSectionTable(patchFD, &eh, sh_tbl);
+
+		int readByts;
+		char *codeSection = extractCodeSection(fd, eh, sh_tbl, &readByts);
+		printf("%d\n", readByts);
+		write(1, codeSection, 10);
+
+		puts("================ CODE CAVE ================");
+
+		int caveSize;
+		char *cave = findLongestCave(codeSection, readByts, &caveSize);
+
+		printf("Cave size = %d\n", caveSize);
+
+		// TODO: Make this copy the org file and only change the fields instead of copynig individualy
+		exit(0);
+
+
 
 		/* Symbol tables :
 		 * sh_tbl[i].sh_type
